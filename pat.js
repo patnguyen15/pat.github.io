2b8f159ec8abd23b699dda860ff7289a1a8aaf5d
patnguyen15 <pat.nguyen15@gmail.com>
2017-12-26 13:09:36 -0600

    added files

---
+++ pat.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+	Typed.new('.element',{
+		strings: ["STUDENT","GAMER","DEVELOPER","PODCAST LISTENER","NBA FAN","NFL FAN","UFC FAN", "BIG BROTHER FAN","MARVEL FAN"],
+		typeSpeed: 0,
+		loop: true,
+		shuffle: true,
+		backDelay: 1000
+	});
+});
+
